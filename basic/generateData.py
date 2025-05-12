94b813e610551c162b70ee21c8bd6d59f908f976
Kaixi66 <kaysenfeng@gmail.com>
2025-05-12 18:41:53 +0800

    1

---
+++ basic/generateData.py
@@ -0,0 +1,8 @@
+import numpy as np
+import pandas as pd
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+
+num_inputs = 2 # 两个特征
+num_examples = 1000 # 一千条数据
+np.random.seed(24) #如果你在代码中间设置了种子，那么只有从这一行之后生成的随机数才会受到影响。
