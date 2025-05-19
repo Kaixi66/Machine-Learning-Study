cf59740719f615562677268c951b477189b5a26d
KaixiMac <kaysenfeng@gmail.com>
2025-05-19 22:26:09 +0800

    1

--- basic/10.skLearn.py
+++ basic/10.skLearn.py
@@ -100,4 +100,129 @@ from sklearn.model_selection import train_test_split
 # Normalization则特指针对单个样本（一行数据）利用其范数进行放缩的过程
 from sklearn import preprocessing
 
-# 标准化 Standardization¶
+# 标准化 Standardization 
+# 包括Z-Score标准化，也包括0-1标准化
+# 可以通过实用函数来进行标准化处理，同时也可以利用评估器来执行标准化过程
+
+# preprocessing模块下的scale函数进行快速的Z-Score标准化处理。
+X = np.arange(9).reshape(3, 3)
+print(X)
+print(preprocessing.scale(X))
+
+
+# 通过评估器的方法进行数据标准化，其实是一种更加通用的选择。
+from sklearn.preprocessing import StandardScaler
+scaler = StandardScaler()
+X = np.arange(15).reshape(5, 3)
+X_train, X_test = train_test_split(X)
+print(X_train, "\n", X_test)
+
+# 此处标准化的评估器的训练结果对输入数据的相关统计量进行了汇总计算
+# 计算了输入数据的均值、标准差等统计量，后续将用这些统计量对各数据进行标准化计算
+scaler.fit(X_train)
+
+# 查看数据各列的标准差，均值，方差, 总共有效的训练条数（列数）
+# 但尚未对任何数据进行修改
+print(scaler.scale_, scaler.mean_, scaler.var_, scaler.n_samples_seen_)
+
+# 利用训练的均值和方差对训练集进行标准化处理
+print(scaler.transform(X_train))
+
+# 利用训练的均值和方差对测试集进行标准化处理
+# 保证训练和测试数据的标准化过程一致，避免数据泄漏（Data Leakage）和模型性能虚高
+print('\n', scaler.transform(X_test))
+
+# 使用fit_transform对输入数据进行直接fit + transform：
+scaler = StandardScaler()
+print('\n', scaler.fit_transform(X_train))
+
+print(scaler.transform(X_test))
+
+# 0-1标准化的函数实现方法
+print("\n\n")
+print(X)
+print(preprocessing.minmax_scale(X))
+
+# 0-1标准化的评估器实现方法
+from sklearn.preprocessing import MinMaxScaler
+print("\n\n")
+scaler = MinMaxScaler()
+print(scaler.fit_transform(X))
+print(scaler.data_max_, scaler.data_min_) # 和前面一样以列的视角
+
+
+
+# 归一化 Normalization
+# 和标准化不同，sklearn中的归一化特指将单个样本（一行数据）放缩为单位范数
+# （1范数或者2范数为单位范数）的过程
+# 用来衡量样本之间相似性
+
+# 1-范数为各分量的绝对值之和
+# 2-范数则为各分量的平方和再开根
+
+# 而sklearn中的Normalization过程，实际上就是将每一行数据视作一个向量，
+# 然后用每一行数据去除以该行数据的1-范数或者2-范数。
+# 具体除以哪个范数，以preprocessing.normalize函数中输入的norm参数为准。
+
+# 1-范数单位化
+print("\n\n",preprocessing.normalize(X, norm='l1'))
+
+# 2-范数单位化
+print("\n\n", preprocessing.normalize(X, norm='l2'))
+
+#调用评估器来实现
+from sklearn.preprocessing import Normalizer
+normlize = Normalizer()
+print("\n\n", normlize.fit_transform(X))
+normlize = Normalizer(norm='l1')
+print(normlize.fit_transform(X))
+
+# 正则化，往往指的是通过在损失函数上加入参数的1-范数或者2-范数的过程，
+# 该过程能够有效避免模型过拟合。
+
+
+# 进行逻辑回归模型训练
+from sklearn.linear_model import LogisticRegression
+#数据准备
+X, y = load_iris(return_X_y=True)
+
+# 实例化模型，使用默认参数
+# 此处设置两个参数，一个是最大迭代次数，另一个则是MvM策略
+clf_test = LogisticRegression(max_iter=1000, multi_class='multinomial')
+clf_test.fit(X, y)
+
+#查看线性方程系数
+print(clf_test.coef_)
+
+print('\n', clf_test.predict(X)[:10])
+
+# 查看结果概率
+print(clf_test.predict_proba(X)[:10])
+
+# 进行准确率计算
+from sklearn.metrics import accuracy_score
+print(accuracy_score(y, clf_test.predict(X)))
+
+
+# 构建机器学习流
+# 将评估器类进行串联形成机器学习流，而不能串联实用函数
+from sklearn.pipeline import make_pipeline
+pipe = make_pipeline(StandardScaler(), LogisticRegression(max_iter=1000))
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42)
+print('\n\n', pipe.fit(X_train, y_train))
+
+# 该过程就相当于两个评估器都进行了训练，然后我们即可使用predict方法，
+# 利用pipe对数据集进行预测，当然实际过程是先（借助训练数据的统计量）进行归一化，
+# 然后再进行逻辑回归模型预测.
+print('\n', pipe.predict(X_test))
+print(pipe.score(X_train, y_train))
+print(pipe.score(X_test, y_test))
+
+# sklearn的模型保存
+# 即可借助joblib包来进行sklearn的模型存储和读取，
+# 相关功能非常简单，我们可以使用dump函数进行模型保存，
+# 使用load函数进行模型读取
+import joblib 
+joblib.dump(pipe, 'pipe.model')
+pipe1 = joblib.load('pipe.model')
+print(pipe1.score(X_train, y_train))
